a053995d4dd68c36046184c6d23122e47de0d9ed
shiyoung77 <shiyang.lu@rutgers.edu>
2021-11-13 16:11:42 -0500

    Minor changes.

--- optimization_test.py
+++ optimization_test.py
@@ -28,7 +28,7 @@ print(res)
 
 if __name__ == '__main__':
     dataset = 'dataset'
-    video_id = "crawling_trial4"
+    video_id = "crawling_trial2"
     prefixes = sorted([i.split('.')[0] for i in os.listdir(os.path.join(dataset, video_id, 'color'))])
 
     data_cfg_module = importlib.import_module(f'{dataset}.{video_id}.config')
@@ -46,7 +46,9 @@ if __name__ == '__main__':
         measured_dist[sensor_id] = []
 
     N = len(prefixes)
-    for idx, prefix in tenumerate(prefixes):
+    first_frame_id = 20
+    for idx in range(N - first_frame_id):
+        prefix = prefixes[idx + first_frame_id]
         info_path = os.path.join(dataset, video_id, 'data', f'{prefix}.json')
         with open(info_path, 'r') as f:
             info = json.load(f)
@@ -62,7 +64,7 @@ if __name__ == '__main__':
     for sensor_id in data_cfg['sensor_to_tendon']:
         row = int(sensor_id) // 3
         col = int(sensor_id) % 3
-        axes[row, col].plot(range(N), predicted_dist[sensor_id], label=f'{sensor_id}-predicted')
-        axes[row, col].plot(range(N), measured_dist[sensor_id], label=f'{sensor_id}-measured')
+        axes[row, col].plot(range(N - first_frame_id), predicted_dist[sensor_id], label=f'{sensor_id}-predicted')
+        axes[row, col].plot(range(N - first_frame_id), measured_dist[sensor_id], label=f'{sensor_id}-measured')
         axes[row, col].legend(loc='upper left')
     plt.show()
