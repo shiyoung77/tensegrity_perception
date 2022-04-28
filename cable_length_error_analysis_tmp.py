fb15e2780c25d5fdb29d5d197955073a0835f01e
Shiyang Lu <shiyanll@uf1b8067517e754.ant.amazon.com>
2022-04-28 16:46:18 -0400

    for python 2.7, untested

--- cable_length_error_analysis_tmp.py
+++ cable_length_error_analysis_tmp.py
@@ -1,5 +1,4 @@
 import os
-import importlib
 import json
 from argparse import ArgumentParser
 from collections import defaultdict
@@ -9,31 +8,49 @@ import scipy.linalg as la
 from matplotlib import pyplot as plt
 
 
+def get_config():
+    cfg = dict()
+    cfg['cable_length_scale'] = 1000
+    cfg['end_cap_colors'] = ['red', 'green', 'blue']
+    cfg['node_to_color'] = ['red', 'red', 'green', 'green', 'blue', 'blue']
+    cfg['color_to_rod'] = {
+        'red': [0, 1],
+        'green': [2, 3],
+        'blue': [4, 5]
+    }
+    cfg['sensor_to_tendon'] = {
+        0: [3, 5],
+        1: [1, 3],
+        2: [1, 5],
+        3: [0, 2],
+        4: [0, 4],
+        5: [2, 4],
+        6: [2, 5],
+        7: [0, 3],
+        8: [1, 4]
+    }
+    return cfg
+
+
 if __name__ == '__main__':
     parser = ArgumentParser("pose evalutation")
     parser.add_argument("--dataset", default="dataset")
-    parser.add_argument("-v", "--video", default="monday_roll15")
+    parser.add_argument("-v", "--video", default="april28_1")
     parser.add_argument("--start_frame", default=0, type=int)
     parser.add_argument("--num_endcaps", default=6, type=int)
     parser.add_argument("--pose_folder", default="poses", type=str)
     parser.add_argument("--mocap_scale", default=1000, type=int, help="scale of measured position (mm by default)")
     args = parser.parse_args()
 
-    data_cfg_module = importlib.import_module(f'{args.dataset}.{args.video}.config')
-    data_cfg = data_cfg_module.get_config(read_cfg=True)
-
-    pos_dict = dict()
-    for i in range(len(data_cfg['node_to_color'])):
-        pos_dict[i] = np.load(os.path.join(args.dataset, args.video, args.pose_folder, f'{i}_pos.npy'))
-
-    N = pos_dict[0].shape[0]
+    N = 100
+    data_cfg = get_config()
 
     measured_dist = defaultdict(list)
     mocap_dist = defaultdict(list)
     measured_errors = defaultdict(list)
 
     for i in range(N):
-        info_path = os.path.join(args.dataset, args.video, 'data', f'{i + args.start_frame:04d}.json')
+        info_path = os.path.join(args.dataset, args.video, 'data', '%04d.json'%(i + args.start_frame))
         with open(info_path, 'r') as f:
             info = json.load(f)
 
@@ -57,12 +74,12 @@ if __name__ == '__main__':
 
     fig, axes = plt.subplots(3, 3)
     fig.set_size_inches(16, 12)
-    fig.suptitle(f"Cable Length Plot")
+    fig.suptitle("Cable Length Plot")
     for sensor_id in data_cfg['sensor_to_tendon']:
         row = int(sensor_id) // 3
         col = int(sensor_id) % 3
-        axes[row, col].plot(range(N), np.array(measured_dist[sensor_id])*100, label=f'{sensor_id}-measured')
-        axes[row, col].plot(range(N), np.array(mocap_dist[sensor_id])*100, label=f'{sensor_id}-mocap')
+        axes[row, col].plot(range(N), np.array(measured_dist[sensor_id])*100, label='%d-measured'%sensor_id)
+        axes[row, col].plot(range(N), np.array(mocap_dist[sensor_id])*100, label='%d-mocap'%sensor_id)
         axes[row, col].legend(loc='best')
         axes[row, col].set_xlabel("frame id")
         axes[row, col].set_ylabel("distance (cm)")
@@ -74,16 +91,16 @@ if __name__ == '__main__':
 
     fig, axes = plt.subplots(3, 3)
     fig.set_size_inches(16, 12)
-    fig.suptitle(f"Cable Length Error Plot\n"
-                 f"mean measured error of all cables (cm) {measured_mean_err:.1f}")
+    fig.suptitle("Cable Length Error Plot\n"
+                 "mean measured error of all cables (cm): %.1f "%measured_mean_err)
     for sensor_id in data_cfg['sensor_to_tendon']:
         row = int(sensor_id) // 3
         col = int(sensor_id) % 3
         axes[row, col].set_xlabel("frame id")
         axes[row, col].set_ylabel("error (cm)")
-        axes[row, col].plot(range(N), measured_errors[sensor_id], label=f'{sensor_id}-measured')
+        axes[row, col].plot(range(N), measured_errors[sensor_id], label='%d-measured'%sensor_id)
         axes[row, col].legend(loc='best')
-        axes[row, col].set_title(f"mean measured error (cm): {np.nanmean(measured_errors[sensor_id]):.1f}", fontsize=10)
+        axes[row, col].set_title("mean measured error (cm): %.1f"%(np.nanmean(measured_errors[sensor_id])), fontsize=10)
         axes[row, col].grid(True)
         axes[row, col].set_ylim(0)
     plt.subplots_adjust(hspace=0.5)
