0ec9a3f909f42efccacf2fe1ea1f0febfe37c1c2
Shiyang Lu <shiyanll@uf1b8067517e754.ant.amazon.com>
2022-04-28 13:59:38 -0400

    add tmp file

---
+++ cable_length_error_analysis_tmp.py
@@ -0,0 +1,91 @@
+import os
+import importlib
+import json
+from argparse import ArgumentParser
+from collections import defaultdict
+
+import numpy as np
+import scipy.linalg as la
+from matplotlib import pyplot as plt
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser("pose evalutation")
+    parser.add_argument("--dataset", default="dataset")
+    parser.add_argument("-v", "--video", default="monday_roll15")
+    parser.add_argument("--start_frame", default=0, type=int)
+    parser.add_argument("--num_endcaps", default=6, type=int)
+    parser.add_argument("--pose_folder", default="poses", type=str)
+    parser.add_argument("--mocap_scale", default=1000, type=int, help="scale of measured position (mm by default)")
+    args = parser.parse_args()
+
+    data_cfg_module = importlib.import_module(f'{args.dataset}.{args.video}.config')
+    data_cfg = data_cfg_module.get_config(read_cfg=True)
+
+    pos_dict = dict()
+    for i in range(len(data_cfg['node_to_color'])):
+        pos_dict[i] = np.load(os.path.join(args.dataset, args.video, args.pose_folder, f'{i}_pos.npy'))
+
+    N = pos_dict[0].shape[0]
+
+    measured_dist = defaultdict(list)
+    mocap_dist = defaultdict(list)
+    measured_errors = defaultdict(list)
+
+    for i in range(N):
+        info_path = os.path.join(args.dataset, args.video, 'data', f'{i + args.start_frame:04d}.json')
+        with open(info_path, 'r') as f:
+            info = json.load(f)
+
+        for sensor_id, (u, v) in data_cfg['sensor_to_tendon'].items():
+            # measured cable length
+            measured_dist[sensor_id].append(info['sensors'][str(sensor_id)]['length'] / 1000)
+
+            # ground truth cable length by motion capture
+            u_pos = info['mocap'][str(u)]
+            v_pos = info['mocap'][str(v)]
+            if u_pos is None or v_pos is None:
+                mocap_dist[sensor_id].append(np.nan)
+            else:
+                u_pos = np.array([u_pos['x'], u_pos['y'], u_pos['z']]) / args.mocap_scale
+                v_pos = np.array([v_pos['x'], v_pos['y'], v_pos['z']]) / args.mocap_scale
+                mocap_dist[sensor_id].append(la.norm(u_pos - v_pos))
+
+            measured_errors[sensor_id].append(np.abs(mocap_dist[sensor_id][-1] - measured_dist[sensor_id][-1])*100)
+
+    os.makedirs(os.path.join(args.dataset, args.video, "error_analysis"), exist_ok=True)
+
+    fig, axes = plt.subplots(3, 3)
+    fig.set_size_inches(16, 12)
+    fig.suptitle(f"Cable Length Plot")
+    for sensor_id in data_cfg['sensor_to_tendon']:
+        row = int(sensor_id) // 3
+        col = int(sensor_id) % 3
+        axes[row, col].plot(range(N), np.array(measured_dist[sensor_id])*100, label=f'{sensor_id}-measured')
+        axes[row, col].plot(range(N), np.array(mocap_dist[sensor_id])*100, label=f'{sensor_id}-mocap')
+        axes[row, col].legend(loc='best')
+        axes[row, col].set_xlabel("frame id")
+        axes[row, col].set_ylabel("distance (cm)")
+    plt.subplots_adjust(hspace=0.5)
+    plt.savefig(os.path.join(args.dataset, args.video, "error_analysis", "cable_length.png"), dpi=150)
+
+    measured_errors_stacked = np.hstack(list(measured_errors.values()))
+    measured_mean_err = np.nanmean(measured_errors_stacked)
+
+    fig, axes = plt.subplots(3, 3)
+    fig.set_size_inches(16, 12)
+    fig.suptitle(f"Cable Length Error Plot\n"
+                 f"mean measured error of all cables (cm) {measured_mean_err:.1f}")
+    for sensor_id in data_cfg['sensor_to_tendon']:
+        row = int(sensor_id) // 3
+        col = int(sensor_id) % 3
+        axes[row, col].set_xlabel("frame id")
+        axes[row, col].set_ylabel("error (cm)")
+        axes[row, col].plot(range(N), measured_errors[sensor_id], label=f'{sensor_id}-measured')
+        axes[row, col].legend(loc='best')
+        axes[row, col].set_title(f"mean measured error (cm): {np.nanmean(measured_errors[sensor_id]):.1f}", fontsize=10)
+        axes[row, col].grid(True)
+        axes[row, col].set_ylim(0)
+    plt.subplots_adjust(hspace=0.5)
+    plt.savefig(os.path.join(args.dataset, args.video, "error_analysis", "cable_length_error.png"), dpi=150)
+    plt.show()
