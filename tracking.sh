7d883df295ea08bcfeba395e5f18db01215462be
Shiyang Lu <shiyanll@uf1b8067517e754.ant.amazon.com>
2022-04-29 21:43:42 -0400

    Add 'end_frame' parameter

--- tracking.sh
+++ tracking.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 DATASET="dataset"
-VIDEO_ID="shiyang3"
+VIDEO_ID="april28_8"
+START_FRAME=0
+END_FRAME=99
 
 python tracking.py \
     --dataset $DATASET \
@@ -9,7 +11,7 @@ python tracking.py \
     --rod_mesh_file "pcd/yale/struct_with_socks_new.ply" \
     --top_endcap_mesh_file "pcd/yale/end_cap_top_new.obj" \
     --bottom_endcap_mesh_file "pcd/yale/end_cap_bottom_new.obj" \
-    --start_frame 0 \
+    --start_frame $START_FRAME \
     --max_correspondence_distances 0.3 0.25 0.2 0.15 0.1 0.06 0.03 \
     --add_dummy_points \
     --num_dummy_points 50 \
@@ -29,14 +31,16 @@ python tracking.py \
 python compute_T_from_cam_to_mocap.py \
     --dataset $DATASET \
     --video $VIDEO_ID \
-    --start_frame 0 \
+    --start_frame $START_FRAME \
+    --end_frame $END_FRAME \
     --num_endcaps 6 \
     --mocap_scale 1000
 
 python pose_error_analysis.py \
     --dataset $DATASET \
     --video $VIDEO_ID \
-    --start_frame 0 \
+    --start_frame $START_FRAME \
+    --end_frame $END_FRAME \
     --num_endcaps 6 \
     --mocap_scale 1000
 
@@ -44,11 +48,12 @@ python cable_length_error_analysis.py \
     --dataset $DATASET \
     --video $VIDEO_ID \
     --pose_folder "poses" \
-    --start_frame 0 \
+    --start_frame $START_FRAME \
+    --end_frame $END_FRAME \
     --num_endcaps 6 \
     --mocap_scale 1000
 
 python compute_mocap_stats.py \
     --dataset $DATASET \
     --video $VIDEO_ID \
-    --start_frame 0 \
+    --start_frame $START_FRAME \
