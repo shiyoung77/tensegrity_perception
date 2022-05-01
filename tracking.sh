015b0e19cbb5fe95ac0d5d5a437bb00611b39186
Shiyang Lu <shiyanll@uf1b8067517e754.ant.amazon.com>
2022-04-30 21:05:31 -0400

    Minor fix

--- tracking.sh
+++ tracking.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 DATASET="dataset"
-VIDEO_ID="april28_8"
+VIDEO_ID="0016"
 START_FRAME=0
-END_FRAME=99
+END_FRAME=1000  # exclusive
 
 python tracking.py \
     --dataset $DATASET \
@@ -12,15 +12,21 @@ python tracking.py \
     --top_endcap_mesh_file "pcd/yale/end_cap_top_new.obj" \
     --bottom_endcap_mesh_file "pcd/yale/end_cap_bottom_new.obj" \
     --start_frame $START_FRAME \
+    --end_frame $END_FRAME \
     --max_correspondence_distances 0.3 0.25 0.2 0.15 0.1 0.06 0.03 \
     --add_dummy_points \
     --num_dummy_points 50 \
     --dummy_weights 0.5 \
     --filter_observed_pts \
     --add_constrained_optimization \
-    --add_physical_constraints \
     --add_ground_constraints \
+    --add_physical_constraints \
     --visualize \
+
+ffmpeg -r 10 -i "$DATASET/$VIDEO_ID/estimation_vis/%04d.png" \
+    -start_number $START_FRAME \
+    -vframes $(expr $END_FRAME - $START_FRAME) \
+    "$DATASET/$VIDEO_ID/estimation.mp4"
 
 # python compute_T_from_cam_to_mocap_manual.py \
 #     --dataset $DATASET \
